5625ff2062420c3f0d970d8636cdf04b79755e71
prdm <pedro.rafael.marinho@gmail.com>
2018-11-19 16:41:25 -0300

    commit modm.R

---
+++ R/modm.R
@@ -0,0 +1,27 @@
+modm <- function(xmin, xup, ymin, yup){
+  browser()
+
+  if(any(is.na(c(xmin, xup, ymin, yup)))){
+    stop("Check the arguments. NA present in at least one of the arguments.")
+  }
+  
+  y_range <- range(y); x_range <- range(x)
+  s0 <- min(y_range/x_range)
+  x_center <- mean(x_range); y_center <- mean(y_range)
+
+  alpha <- cov(x_center, y_center)/var(x_center)
+  beta <- min(s0, max(0, cov(x_range, y_range)/var(x_range)))
+  
+  Bmin <- (mean(mean(y)) - alpha*mean(x_center)) - (mean(y_range) - beta*mean(x_range))
+  Bmax <- (mean(mean(y)) - alpha*mean(x_center)) + (mean(y_range) - beta*mean(x_range))
+  
+  gamma <- mean(Bmax, Bmin)
+  delta <- (Bmax - Bmin)/2
+  
+  Ymin_est <- alpha * x_center + beta * x_range - (gamma - delta)
+  Xmin_est <- alpha * x_center + beta * x_range + (gamma - delta)
+  
+  return(list(Ymin_est = Ymin_est, Xmin_est = Xmin_est, alpha = alpha,
+              beta = beta, Bmin = Bmin, Bmax = Bmax, gramma = gramma,
+              delta = deltra))
+}
